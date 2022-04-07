a7b5610060487b3b11ee6891dd39ab5a67079e11
Monika Jhingan <jhinganmonika750@gmail.com>
2022-04-07 23:56:15 +0530

    todo updated homepage, navbar

--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React, { StrictMode } from "react";
 import "./index.css";
 import App from "./App";
-import { makeServer } from "./server";
+import reportWebVitals from "./reportWebVitals";
+import * as ReactDOMClient from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
+import { TodoProvider } from "./context/todo-context";
 
-// Call make Server
-makeServer();
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+const root = ReactDOMClient.createRoot(document.getElementById("root"));
+root.render(
+  <StrictMode>
+    <Router>
+      <TodoProvider>
+        <App />
+      </TodoProvider>
+    </Router>
+  </StrictMode>
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
